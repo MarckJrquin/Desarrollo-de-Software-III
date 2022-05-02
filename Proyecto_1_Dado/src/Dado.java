6f8b048253aaf401b9eec3ec5658d61e48677af8
MarckJrquin <marck0208@outlook.com>
2022-05-01 19:17:51 -0500

    commit para establecer la version 1

--- Proyecto_1_Dado/src/Dado.java
+++ Proyecto_1_Dado/src/Dado.java
@@ -5,20 +5,8 @@ public class Dado {
         this.numeroCara = 6;
     }
 
-    public Dado(int numeroCara){
-        this.numeroCara = numeroCara;
-    }
-
     public int tirarDado(){
         return this.generaNumeroAleatorio(1, numeroCara);
-    }
-
-    public int[] tirarDadoNVeces(int n){
-        int[] valores = new int[n];
-        for(int i=0;i<valores.length;i++){
-            valores[i]=tirarDado();
-        }
-        return valores;
     }
 
     private int generaNumeroAleatorio(int minimo,int maximo){
--- Proyecto_1_Dado/src/JuegoDeDados.java
+++ Proyecto_1_Dado/src/JuegoDeDados.java
@@ -3,29 +3,35 @@ public class JuegoDeDados {
 
         Dado dado = new Dado();
 
+        System.out.println("[ Proyecto N°1 --------------------------------------- ]");
+        System.out.println("[ Proyecto de Herencia y Polimorfismo - Juego de Dados ]\n");
+
         int puntosJ1=0;
         int puntosJ2=0;
+        int aux1=0;
+        int aux2=0;
 
         for(int i = 0; i < (2*2); i++){
             if(i%2==0){
+                aux1=puntosJ1;
                 puntosJ1 += dado.tirarDado();
-                System.out.println("Lazamiento del jugador 1: "+puntosJ1);
+                System.out.println("[ ] Lazamiento del jugador 1: "+(puntosJ1-aux1));
             }else{
+                aux2=puntosJ2;
                 puntosJ2 += dado.tirarDado();
-                System.out.println("Lazamiento del jugador 2: "+puntosJ2);
+                System.out.println("[ ] Lazamiento del jugador 2: "+(puntosJ2-aux2));
             }
         }
 
-        System.out.println("Puntos de Jugador 1: "+puntosJ1);
-        System.out.println("Puntos de Jugador 2: "+puntosJ2);
+        System.out.println("\n - Puntos de Jugador 1: "+puntosJ1);
+        System.out.println(" - Puntos de Jugador 2: "+puntosJ2);
 
         if(puntosJ1 == puntosJ2){
-            System.out.println("Empate");
+            System.out.println("\nEmpate!");
         }else if(puntosJ1 > puntosJ2){
-            System.out.println("Jugador 1 Gana");
+            System.out.println("\nGanador: Jugador 1");
         }else{
-            System.out.println("Jugador 2 Gana");
+            System.out.println("\nGanador: Jugador 2");
         }
-
     }
 }
